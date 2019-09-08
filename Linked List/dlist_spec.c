cac228be52b94f2bd258ed102ac54a8cef8e2304
anchal-panda98 <54840381+anchal-panda98@users.noreply.github.com>
2019-09-08 16:54:32 +0530

    Add files via upload

---
+++ Linked List/dlist_spec.c
@@ -0,0 +1,273 @@
+/*
+ * C Program to Implement Doubly Linked List using Dynamic Memory Allocation
+ */
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * Node Declaration
+ */
+struct node
+{
+    struct node *prev;
+    int n;
+    struct node *next;
+}*h,*temp,*temp1,*temp2,*temp4;
+
+int count = 0;
+
+void create();
+void insert_beg();
+void insert_end();
+void insert_pos();
+void traverse_beg();
+void search();
+void delete_element();
+
+/*
+ * Main Function:
+ * 1. Insert at begining
+ * 2. Insert at end
+ * 3. Insert at a given position
+ * 4. Delete from a given position
+ * 5. Traverse the list
+ * 6. Search for an element
+ */
+void main()
+{
+    int ch;
+ 
+    h = NULL;
+    temp = temp1 = NULL;
+ 
+    printf("\n 1 - Insert at beginning");
+    printf("\n 2 - Insert at end");
+    printf("\n 3 - Insert at a given position");
+    printf("\n 4 - Delete from a given position");
+    printf("\n 5 - Display from beginning");
+    printf("\n 6 - Search for element");
+    printf("\n 7 - Exit");
+ 
+    while (1)
+    {
+        printf("\n Enter choice : ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            insert_beg();
+            break;
+        case 2:
+            insert_end();
+            break;
+        case 3:
+            insert_pos();
+            break;
+        case 4:
+            delete_element();
+            break;
+        case 5:
+            traverse_beg();
+            break;
+        case 6:
+            search();
+            break;
+        case 7:
+            exit(0);
+        default:
+            printf("\n Wrong choice menu");
+        }
+    }
+}
+
+/* To create an empty node */
+void create()
+{
+    int data;
+ 
+    temp =(struct node *)malloc(1*sizeof(struct node));
+    temp->prev = NULL;
+    temp->next = NULL;
+    printf("\n Enter value to node : ");
+    scanf("%d", &data);
+    temp->n = data;
+    count++;
+}
+ 
+/*  To insert at beginning */
+void insert_beg()
+{
+    if (h == NULL)
+    {
+        create();
+        h = temp;
+        temp1 = h;
+    }
+    else
+    {
+        create();
+        temp->next = h;
+        h->prev = temp;
+        h = temp;
+    }
+}
+ 
+/* To insert at end */
+void insert_end()
+{
+    if (h == NULL)
+    {
+        create();
+        h = temp;
+        temp1 = h;
+    }
+    else
+    {
+        create();
+        temp1->next = temp;
+        temp->prev = temp1;
+        temp1 = temp;
+    }
+}
+ 
+/* To insert at any position */
+void insert_pos()
+{
+    int pos, i = 2;
+ 
+    printf("\n Enter position to be inserted : ");
+    scanf("%d", &pos);
+    temp2 = h;
+ 
+    if ((pos < 1) || (pos >= count + 1))
+    {
+        printf("\n Position out of range to insert");
+        return;
+    }
+    if ((h == NULL) && (pos != 1))
+    {
+        printf("\n Empty list cannot insert other than 1st position");
+        return;
+    }
+    if ((h == NULL) && (pos == 1))
+    {
+        create();
+        h = temp;
+        temp1 = h;
+        return;
+    }
+    else
+    {
+        while (i < pos)
+        {
+            temp2 = temp2->next;
+            i++;
+        }
+        create();
+        temp->prev = temp2;
+        temp->next = temp2->next;
+        temp2->next->prev = temp;
+        temp2->next = temp;
+    }
+}
+ 
+/* To delete an element */
+void delete_element()
+{
+    int i = 1, pos;
+ 
+    printf("\n Enter position to be deleted : ");
+    scanf("%d", &pos);
+    temp2 = h;
+ 
+    if ((pos < 1) || (pos >= count + 1))
+    {
+        printf("\n Error : Position out of range to delete");
+        return;
+    }
+    if (h == NULL)
+    {
+        printf("\n Error : Empty list no elements to delete");
+        return;
+    }
+    else
+    {
+        while (i < pos)
+        {
+            temp2 = temp2->next;
+            i++;
+        }
+        if (i == 1)
+        {
+            if (temp2->next == NULL)
+            {
+                printf("Node deleted from list");
+                free(temp2);
+                temp2 = h = NULL;
+                return;
+            }
+        }
+        if (temp2->next == NULL)
+        {
+            temp2->prev->next = NULL;
+            free(temp2);
+            printf("Node deleted from list");
+            return;
+        }
+        temp2->next->prev = temp2->prev;
+        if (i != 1)
+            temp2->prev->next = temp2->next;    /* Might not need this statement if i == 1 check */
+        if (i == 1)
+            h = temp2->next;
+        printf("\n Node deleted");
+        free(temp2);
+    }
+    count--;
+}
+ 
+/* Traverse from beginning */
+void traverse_beg()
+{
+    temp2 = h;
+ 
+    if (temp2 == NULL)
+    {
+        printf("List empty to display \n");
+        return;
+    }
+    printf("\n Linked list elements from begining : ");
+ 
+    while (temp2->next != NULL)
+    {
+        printf(" %d ", temp2->n);
+        temp2 = temp2->next;
+    }
+    printf(" %d ", temp2->n);
+}
+ 
+/* To search for an element in the list */
+void search()
+{
+    int data, count = 0;
+    temp2 = h;
+ 
+    if (temp2 == NULL)
+    {
+        printf("\n Error : List empty to search for data");
+        return;
+    }
+    printf("\n Enter value to search : ");
+    scanf("%d", &data);
+    while (temp2 != NULL)
+    {
+        if (temp2->n == data)
+        {
+            printf("\n Data found in %d position",count + 1);
+            return;
+        }
+        else
+             temp2 = temp2->next;
+            count++;
+    }
+    printf("\n Error : %d not found in list", data);
+}
